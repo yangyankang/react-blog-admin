932eec982eb14e8d3b6d76a21e5d441ae04fbbf2
杨焱康 <704799639@qq.com>
2020-05-27 18:24:42 +0800

    增加tslint

--- src/components/hello.tsx
+++ src/components/hello.tsx
@@ -1,8 +1,8 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
 
-export interface HelloProps { compiler: string; framework: string; }
+export interface IHelloProps { compiler: string; framework: string }
 
-export class Hello extends Component<HelloProps, {}> {
+export class Hello extends Component <IHelloProps, {}> {
   render() {
     return <h1>hello from {this.props.compiler} and {this.props.framework}!</h1>
   }
